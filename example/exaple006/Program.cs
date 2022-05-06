1c18299e00999597eb9197b7979170f54d20e484
Пётр <dimok392@mail.ru>
2022-05-06 18:31:06 +0300

    example/exaple006

---
+++ example/exaple006/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(0,11);
+    }
+}
+void Print(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+         Console.Write(array[i]+" ");
+    }
+   Console.WriteLine();
+}
+int [] mass = new int [10];
+FillArray(mass);
+Print(mass);
+int []mass2 = new int [mass.Length];
+for (int i = 0; i < mass2.Length; i++)
+{
+    mass2[i]=mass[i];
+}
+Print(mass2);
