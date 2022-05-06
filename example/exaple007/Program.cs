e72c068b1494996b4f57df35540e65d9290fccd4
Пётр <dimok392@mail.ru>
2022-05-06 19:06:36 +0300

    example/exaple007

---
+++ example/exaple007/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел " );
+string d = Console.ReadLine();
+string [] arr = d.Split(" ");
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    int m = int.Parse(arr[i]);
+    if (m>0)
+    count=count+1;
+    // Console.Write(sum );
+    // // Console.Write(arr[i] + ",");
+}
+ Console.Write(count);
