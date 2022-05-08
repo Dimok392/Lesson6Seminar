36620dd5d61a0c1f5b6f27ec5bd2d27007cb8cc8
Пётр <dimok392@mail.ru>
2022-05-08 12:52:11 +0300

    example/exaple008/

--- example/exaple008/Program.cs
+++ example/exaple008/Program.cs
@@ -13,41 +13,28 @@
 // Console.WriteLine(x);
 // Console.WriteLine(y1);
 // Console.WriteLine(y2);
-
-
-// double k1 = 5;
-// double k2 = 9;
-// double b1 = 2;
-// double b2 = 4;
-// double y1 ;
-// double y2 ;
-// double x1 = -20;
-// double x2 = 41;
-// while(x1<x2)
-// {
-// y1 =  k1 *x1 + b1;
-// y2 =  k2 *x1 + b2;
-// if (y1==y2)
-// {
-//     Console.WriteLine(x1);
-//     Console.WriteLine(y1);
-// }
-// x1 = x1+0.25;
-// }
 double k1 = 5;
 double k2 = 9;
 double b1 = 2;
 double b2 = 4;
-double x=(b2-b1)/(k1-k2);
-double y=0;
-double Function (double x, double k1, double b1)
+
+double FunctionX (double m, double r, double o,double l)
 {
-   double y = k1*x + b1;
-return y;
+   double Ox = (m-r)/(o-l);
+   return Ox;
+}
+double FunctionY (double Ox, double k, double b)
+{
+   double Oy = k*Ox + b;
+return Oy;
 }
 void Print(double y)
 {
  Console.Write(y+" ");
 }
-Function(x,k1,b1);
+double x = FunctionX(b2,b1,k1,k2);
+Print(x);
+double y=FunctionY(x,k1,b1);
 Print(y);
+
+
